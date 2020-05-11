aa70612611e99d295d184e6624ed7dc8fc1696da
Charles <charleslcsantos@gmail.com>
2020-05-10 21:39:45 -0300

    added promise on getPlants and refactored tests

--- assets/js/Plants.js
+++ assets/js/Plants.js
@@ -1,26 +1,28 @@
 const SUNLIGHT_OPTIONS = ["high", "low", "no"];
 const WATER_OPTIONS = ["daily", "regularly", "rarely"];
 const PET_OPTIONS = ["true", "false"];
-const apiUrl = `https://6nrr6n9l50.execute-api.us-east-1.amazonaws.com/default/front-plantTest-service?sun=high&water=rarely&pets=false`;
+const API_URL = `https://6nrr6n9l50.execute-api.us-east-1.amazonaws.com/default/front-plantTest-service?sun=high&water=rarely&pets=false`;
 
 function getPlants(sunlight, water, pet) {
-  if (!sunlight || !water || !pet) {
-    return new Error("You must select sunlight, water and pet options");
-  }
+  return new Promise((resolve, reject) => {
+    if (!sunlight || !water || !pet) {
+      reject(new Error("You must select sunlight, water and pet options"));
+    }
 
-  if (!SUNLIGHT_OPTIONS.includes(sunlight)) {
-    return new Error("The sunlight parameter is not valid");
-  }
+    if (!SUNLIGHT_OPTIONS.includes(sunlight)) {
+      reject(new Error("The sunlight parameter is not valid"));
+    }
 
-  if (!WATER_OPTIONS.includes(water)) {
-    return new Error("The water parameter is not valid");
-  }
+    if (!WATER_OPTIONS.includes(water)) {
+      reject(new Error("The water parameter is not valid"));
+    }
 
-  if (!PET_OPTIONS.includes(pet)) {
-    return new Error("The pet parameter is not valid");
-  }
+    if (!PET_OPTIONS.includes(pet)) {
+      reject(new Error("The pet parameter is not valid"));
+    }
 
-  return fetch(apiUrl);
+    resolve(fetch(API_URL));
+  });
 }
 
 export const Plants = {
--- test/Plants.test.js
+++ test/Plants.test.js
@@ -5,28 +5,32 @@ const expect = require("chai").expect;
 const Plants = require("../assets/js/Plants.js").Plants;
 
 describe("#Plants", () => {
-  it("should return error when not select the form options", () => {
-    const res = Plants.getPlants();
-
-    expect(res).to.be.an("error");
+  it("should return error when not select the form options", (done) => {
+    Plants.getPlants().catch((res) => {
+      done();
+      expect(res).to.be.an("error");
+    });
   });
 
-  it("should return error if sunlight parameter is not valid", () => {
-    const res = Plants.getPlants("option", "rarely", "false");
-
-    expect(res).to.be.an("error");
+  it("should return error if sunlight parameter is not valid", (done) => {
+    Plants.getPlants("option", "rarely", "false").catch((res) => {
+      done();
+      expect(res).to.be.an("error");
+    });
   });
 
-  it("should return error if water parameter is not valid", () => {
-    const res = Plants.getPlants("high", "option", "false");
-
-    expect(res).to.be.an("error");
+  it("should return error if water parameter is not valid", (done) => {
+    Plants.getPlants("high", "option", "false").catch((res) => {
+      done();
+      expect(res).to.be.an("error");
+    });
   });
 
-  it("should return error if pet parameter is not valid", () => {
-    const res = Plants.getPlants("high", "rarely", "option");
-
-    expect(res).to.be.an("error");
+  it("should return error if pet parameter is not valid", (done) => {
+    Plants.getPlants("high", "rarely", "option").catch((res) => {
+      done();
+      expect(res).to.be.an("error");
+    });
   });
 
   it("should return 200 status if all options has selected", (done) => {
